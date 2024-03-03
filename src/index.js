703259daadce491122838dae59181b8a352f3293
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-03-03 11:25:24 +0100

    Implemented basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "Generating poetry poem....";
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
